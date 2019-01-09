1b57b413e2bf15315f02903a0c2e77b45d815864
Alexander Kovtun <akovtun@gmail.com>
2019-01-09 18:29:25 +0200

    relocate module

--- App.js
+++ App.js
@@ -12,52 +12,18 @@ import {
   Button,
 } from 'react-native';
 
-import MapView from './src/mapView.js';
+//import MapView from './src/nativeMap';
+import Stepper from './src/nativeStepper';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      isZoomEnabled: false,
-      region: {
-        latitude: 49.983058,
-        longitude: 36.308151,
-        latitudeDelta: 0.4,
-        longitudeDelta: 0.4,
-      },
-    };
   }
-
-  _changeZoomPermission = () => {
-    this.setState({
-      isZoomEnabled: !this.state.isZoomEnabled,
-    });
-  };
-
-  _onRegionChange = event => {};
 
   render() {
     return (
       <SafeAreaView style={{flex: 1, backgroundColor: 'grey'}}>
-        <View
-          style={{
-            width: '100%',
-            marginBottom: 40,
-            height: 100,
-            backgroundColor: 'green',
-          }}>
-          <Button
-            onPress={this._changeZoomPermission}
-            title={'change zoom permission'}
-            color={'cyan'}
-          />
-        </View>
-        <MapView
-          style={{flex: 1}}
-          region={this.state.region}
-          zoomEnabled={this.state.isZoomEnabled}
-          onRegionChange={this._onRegionChange}
-        />
+        <Stepper />
       </SafeAreaView>
     );
   }
