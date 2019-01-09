06f83230660d6da2f7286fa16956b8922ed8c21e
Alexander Kovtun <akovtun93@gmail.com>
2019-01-09 13:03:41 +0200

    adding react-native-fast image and react native svg via cocoapods (#3)

--- App.js
+++ App.js
@@ -9,7 +9,11 @@ import {
   Text,
   View,
   SafeAreaView,
+  Dimensions,
 } from 'react-native';
+
+import Svg, {Circle} from 'react-native-svg';
+import FastImage from 'react-native-fast-image';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,15 +22,25 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const {width, height} = Dimensions.get('window');
+
 export default class App extends Component {
   constructor(props) {
     super(props);
   }
 
-    render() {
-    
+  render() {
     return (
       <SafeAreaView style={styles.container}>
+        <FastImage
+          style={{ width: 100, height: 100 }}
+          source={{
+            uri: 'https://unsplash.it/400/400?image=1',
+            headers: {Authorization: 'someAuthToken'},
+            priority: FastImage.priority.normal,
+          }}
+          resizeMode={FastImage.resizeMode.contain}
+        />
         <View
           style={{
             width: 200,
@@ -36,14 +50,10 @@ export default class App extends Component {
           }}>
           <Text> some text </Text>
         </View>
-        <View
-          style={{width: '100%'}}
-          scrollEventThrottle={1}>
+        <View style={{width: '100%'}} scrollEventThrottle={1}>
           <Text style={styles.welcome}>Welcome to React Native!</Text>
           <Text style={styles.instructions}>To get started, edit App.js</Text>
-          <View
-            style={{height: 150, width: '100%', backgroundColor: 'blue'}}
-          />
+          <View style={{height: 150, width: '100%', backgroundColor: 'blue'}} />
           <Text style={styles.instructions}>{instructions}</Text>
         </View>
       </SafeAreaView>
